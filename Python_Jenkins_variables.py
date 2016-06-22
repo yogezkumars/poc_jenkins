c95a35d22486abe930fb1d2088cd0b65364bf699
yogezkumars <yogezkumars@gmail.com>
2016-06-22 15:07:02 +0530

    modified

--- Python_Jenkins_variables.py
+++ Python_Jenkins_variables.py
@@ -1,7 +1,7 @@
 import os
 
-job_name = os.environ[JOB_NAME]
-Status = os.environ[EXECUTOR_NUMBER]
+job_name = os.environ[$JOB_NAME]
+Status = os.environ[$EXECUTOR_NUMBER]
 print "Yogesh"
 print "Job Name: " + job_name
 print "Job Status: " + Status
